80632b214487e2c41a0db278a2d2bfab45f3ac70
Musilah <nataleigh.nk@gmail.com>
2025-01-24 13:50:52 +0300

    fix favicon
    Signed-off-by: Musilah <nataleigh.nk@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,21 @@ import Header from '@/components/ui/header';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { getImageUrl } from '@/lib/getImageUrl';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Magistrala',
   description: 'Connect and Visualize your Devices',
-};
+  icons: {
+    icon: [
+      {
+        url: getImageUrl("favicon.png")
+      }
+    ]
+  }
+  };
 
 export default function RootLayout({
   children,
