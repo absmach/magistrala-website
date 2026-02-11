b939f062b91d33994dc05218da9f2ec338bad25d
ianmuchyri <ianmuchiri8@gmail.com>
2026-02-11 16:57:38 +0300

    add logo to navbars
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,18 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<"/docs">) {
+  const base = baseOptions();
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      {...base}
+      tree={source.getPageTree()}
+      links={base.links?.filter((item) => item.type === "icon")}
+      nav={{
+        ...base.nav,
+      }}
+    >
       {children}
     </DocsLayout>
   );
