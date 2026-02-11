353d2dbfc802304d439dc97d3e4328adb7a5f9a3
ianmuchyri <ianmuchiri8@gmail.com>
2026-02-11 18:16:30 +0300

    initialize shadcn
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Inter, Roboto } from 'next/font/google';
 import { Provider } from '@/components/provider';
 import './global.css';
+import { cn } from "@/lib/utils";
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   subsets: ['latin'],
@@ -8,7 +11,7 @@ const inter = Inter({
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={cn(inter.className, roboto.variable)} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <Provider>{children}</Provider>
       </body>
