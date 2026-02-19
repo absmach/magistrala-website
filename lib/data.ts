736ccd3fffbf04835e038ae8912c42700346c008
wambui-pixel <80099068+wambui-pixel@users.noreply.github.com>
2026-02-19 17:54:56 +0300

    fix org email (#66)
    Signed-off-by: wambui-pixel <kiokowambui015@gmail.com>

---
+++ lib/data.ts
@@ -0,0 +1,23 @@
+export const socialLinks = {
+  twitter: "https://twitter.com/absmach",
+  linkedin: "https://www.linkedin.com/company/abstract-machines",
+  community: "https://matrix.to/#/#magistrala:matrix.org",
+};
+
+export const relatedProducts = [
+  {
+    name: "Propeller",
+    url: "https://propeller.absmach.eu/",
+    description: "WebAssembly orchestrator for Cloud-Edge workloads",
+  },
+  {
+    name: "FluxMQ",
+    url: "https://fluxmq.absmach.eu/",
+    description: "A high-performance, multi-protocol message broker",
+  },
+  {
+    name: "Abstract Machines Hardware",
+    url: "https://hardware.absmach.eu/",
+    description: "Modular IoT Gateway Solutions",
+  },
+];
